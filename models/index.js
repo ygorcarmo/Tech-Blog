abf561b22f82dacb2715528864d15c6939b347f4
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-11-28 10:09:22 +1100

    update models/index.js

--- models/index.js
+++ models/index.js
@@ -5,9 +5,26 @@ User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
 
-Project.belongsTo(User, {
+Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+Comment.belongsTo( Post, {
+    foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
