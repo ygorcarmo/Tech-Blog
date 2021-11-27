dd473307f63e6b03c6817d387d17fc5d0dc1ea3d
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-11-28 09:39:27 +1100

    added Post controller

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+
+// sequelize db connection
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
