99a5caf90d90ec1b82eb3a5d9e4ae26dc1ec2ccc
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-11-28 11:14:18 +1100

    added db seeds and added timestamp to comments

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 
@@ -36,6 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routes);
 
 // Starts the server to begin listening
 sequelize.sync({ force: false}).then(() => {app.listen(PORT, () => {
